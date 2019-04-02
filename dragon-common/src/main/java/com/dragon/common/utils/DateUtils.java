e30b2fcae45b249bd353a5059e4a796d7832a1ce
alex <yumin_860619@126.com>
2019-04-02 18:33:54 +0800

    initial commit

---
+++ dragon-common/src/main/java/com/dragon/common/utils/DateUtils.java
@@ -0,0 +1,89 @@
+package com.dragon.common.utils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.WeakHashMap;
+
+public class DateUtils {
+
+    private static WeakHashMap<String, SimpleDateFormat> simpleDateFormats = new WeakHashMap<String, SimpleDateFormat>();
+
+    private static String DEFAULT_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    private static String DEFAULT_LONGFORMAT = "yyyyMMddHHmmss";
+
+    private DateUtils() {
+    }
+
+    private static synchronized SimpleDateFormat getSimpleDateFormat(String format) {
+        SimpleDateFormat sdf = simpleDateFormats.get(format);
+        if(sdf == null) {
+            sdf = new SimpleDateFormat(format);
+            simpleDateFormats.put(format, sdf);
+        }
+        return sdf;
+    }
+
+    public static String format(Date date, String format) {
+        SimpleDateFormat sdf = getSimpleDateFormat(format);
+        synchronized (sdf) {
+            return sdf.format(date);
+        }
+    }
+
+    public static String format(Date date) {
+        return format(date, DEFAULT_FORMAT);
+    }
+
+    public static Date parse(String date, String format) {
+        SimpleDateFormat sdf = getSimpleDateFormat(format);
+        try {
+            synchronized (sdf) {
+                return sdf.parse(date);
+            }
+        } catch (ParseException e) {
+            throw new RuntimeException("Parse date " + date + " with format " + format + " failed.", e);
+        }
+    }
+
+    public static Date parse(String date) {
+        return parse(date, DEFAULT_FORMAT);
+    }
+
+    public static int formatToInt(Date date, String format) {
+        return Integer.parseInt(format(date, format));
+    }
+
+    public static long formatToLong(Date date, String format) {
+        return Long.parseLong(format(date, format));
+    }
+
+    public static long formatToLong(Date date) {
+        return Long.parseLong(format(date, DEFAULT_LONGFORMAT));
+    }
+
+    public static Date parseFromInt(int date, String format) {
+        return parse(String.valueOf(date), format);
+    }
+
+    public static Date add(Date date, int field, int amount) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(field, amount);
+        return calendar.getTime();
+    }
+
+    /**
+     * 日期相加
+     * @param date
+     * @param day
+     * @return
+     */
+    public static Date addDate(Date date, int day) {
+        Calendar c = Calendar.getInstance();
+        c.setTimeInMillis(c.getTimeInMillis() + day*24*3600*1000);
+        return c.getTime();
+    }
+
+}
---
+++ dragon-facade/dragon-facade-api/src/main/java/com/dragon/DragonFacadeApiApplication.java
@@ -0,0 +1,13 @@
+package com.dragon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DragonFacadeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DragonFacadeApiApplication.class, args);
+	}
+
+}
